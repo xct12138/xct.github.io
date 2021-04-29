a7bb60560bd7be8a0ea2227d3d949758ea87895c
xct <3031371167@qq.com>
2021-04-29 21:14:58 +0800

    new

--- src/servlet/Servlet1.java
+++ src/servlet/Servlet1.java
@@ -1,2 +1,18 @@
-package servlet;public class Servlet1 {
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet1")
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("hello world!!!");
+    }
 }

da34eb71650b1fa535237199a1958b70f55bb93c
xct <3031371167@qq.com>
2021-05-02 23:46:01 +0800

    new

--- src/sql/SqlConfig.java
+++ src/sql/SqlConfig.java
@@ -5,15 +5,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class SqlConfig {
-    public static String[] getLoginX() throws FileNotFoundException {
+    public static String[] getLoginX() {
         String[] s = new String[3];
-        Scanner scanner=new Scanner(new FileInputStream("SqlLogin.conf"));
-        int i=0;
-        while (scanner.hasNext()){
-            String s1 = scanner.nextLine();
-            if (!"".equals(s1)){
-                s[i++]=s1;
+        Scanner scanner= null;
+        try {
+            scanner = new Scanner(new FileInputStream("src/sql/SqlLogin.conf"));
+            int i=0;
+            while (scanner.hasNext()){
+                String s1 = scanner.nextLine();
+                if (!"".equals(s1)){
+                    s[i++]=s1;
+                }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            s[0]="xct";
+            s[1]="1234";
+            s[2]="tdb1";
         }
         return s;
     }
